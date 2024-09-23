7b646c8d69176af817f9448b3594f4f47b436c8e
Aleš Kučera <kuceral4@fel.cvut.cz>
2024-09-23 08:26:19 +0200

    Add brute eval

--- src/scripts/test.py
+++ src/scripts/test.py
@@ -1,4 +1,5 @@
 import os
+import argparse
 
 from tests import Assignment
 from tests import test_assignment_1_1, test_assignment_1_2, test_assignment_1_3
@@ -16,82 +17,92 @@ from tests import (
     test_assignment_4_4,
 )
 
+PROJECT_DIR = os.path.abspath(
+    os.path.join(os.path.dirname(os.path.abspath(__file__)), os.pardir, os.pardir)
+)
 
-SEED = 69
-GENERATE = False
 
-SCRIPTS_DIR = os.path.dirname(os.path.abspath(__file__))
-PROJECT_DIR = os.path.abspath(os.path.join(SCRIPTS_DIR, os.pardir, os.pardir))
-SRC_DIR = os.path.join(PROJECT_DIR, "src", "assignments")
-
-ASSIGNMENTS = [
-    Assignment(
-        name="assignment_1_1",
-        test_func=test_assignment_1_1,
-        verification_file=os.path.join(PROJECT_DIR, "data/tests/assignment_1_1.npy"),
-    ),
-    Assignment(
-        name="assignment_1_2",
-        test_func=test_assignment_1_2,
-        verification_file=os.path.join(PROJECT_DIR, "data/tests/assignment_1_2.npy"),
-    ),
-    Assignment(
-        name="assignment_1_3",
-        test_func=test_assignment_1_3,
-        verification_file=os.path.join(PROJECT_DIR, "data/tests/assignment_1_3.npy"),
-    ),
-    Assignment(
-        name="assignment_2_1",
-        test_func=test_assignment_2_1,
-        verification_file=os.path.join(PROJECT_DIR, "data/tests/assignment_2_1.npy"),
-    ),
-    Assignment(
-        name="assignment_3_1",
-        test_func=test_assignment_3_1,
-        verification_file=os.path.join(PROJECT_DIR, "data/tests/assignment_3_1.pt"),
-    ),
-    Assignment(
-        name="assignment_3_2",
-        test_func=test_assignment_3_2,
-        verification_file=os.path.join(PROJECT_DIR, "data/tests/assignment_3_2.pt"),
-    ),
-    Assignment(
-        name="assignment_3_3",
-        test_func=test_assignment_3_3,
-        verification_file=os.path.join(PROJECT_DIR, "data/tests/assignment_3_3.pt"),
-    ),
-    Assignment(
-        name="assignment_3_4",
-        test_func=test_assignment_3_4,
-        verification_file=os.path.join(PROJECT_DIR, "data/tests/assignment_3_4.pt"),
-    ),
-    Assignment(
-        name="assignment_4_1",
-        test_func=test_assignment_4_1,
-        verification_file=os.path.join(PROJECT_DIR, "data/tests/assignment_4_1.pt"),
-    ),
-    Assignment(
-        name="assignment_4_2",
-        test_func=test_assignment_4_2,
-        verification_file=os.path.join(PROJECT_DIR, "data/tests/assignment_4_2.pt"),
-    ),
-    Assignment(
-        name="assignment_4_3",
-        test_func=test_assignment_4_3,
-        verification_file=os.path.join(PROJECT_DIR, "data/tests/assignment_4_3.pt"),
-    ),
-    Assignment(
-        name="assignment_4_4",
-        test_func=test_assignment_4_4,
-        verification_file=os.path.join(PROJECT_DIR, "data/tests/assignment_4_4.pt"),
-    ),
-]
+def create_assignments(dataset_dir: str) -> list:
+    return [
+        Assignment(
+            name="assignment_1_1",
+            test_func=test_assignment_1_1,
+            verification_file=os.path.join(dataset_dir, "assignment_1_1.npy"),
+        ),
+        Assignment(
+            name="assignment_1_2",
+            test_func=test_assignment_1_2,
+            verification_file=os.path.join(dataset_dir, "assignment_1_2.npy"),
+        ),
+        Assignment(
+            name="assignment_1_3",
+            test_func=test_assignment_1_3,
+            verification_file=os.path.join(dataset_dir, "assignment_1_3.npy"),
+        ),
+        Assignment(
+            name="assignment_2_1",
+            test_func=test_assignment_2_1,
+            verification_file=os.path.join(dataset_dir, "assignment_2_1.npy"),
+        ),
+        Assignment(
+            name="assignment_3_1",
+            test_func=test_assignment_3_1,
+            verification_file=os.path.join(dataset_dir, "assignment_3_1.pt"),
+        ),
+        Assignment(
+            name="assignment_3_2",
+            test_func=test_assignment_3_2,
+            verification_file=os.path.join(dataset_dir, "assignment_3_2.pt"),
+        ),
+        Assignment(
+            name="assignment_3_3",
+            test_func=test_assignment_3_3,
+            verification_file=os.path.join(dataset_dir, "assignment_3_3.pt"),
+        ),
+        Assignment(
+            name="assignment_3_4",
+            test_func=test_assignment_3_4,
+            verification_file=os.path.join(dataset_dir, "assignment_3_4.pt"),
+        ),
+        Assignment(
+            name="assignment_4_1",
+            test_func=test_assignment_4_1,
+            verification_file=os.path.join(dataset_dir, "assignment_4_1.pt"),
+        ),
+        Assignment(
+            name="assignment_4_2",
+            test_func=test_assignment_4_2,
+            verification_file=os.path.join(dataset_dir, "assignment_4_2.pt"),
+        ),
+        Assignment(
+            name="assignment_4_3",
+            test_func=test_assignment_4_3,
+            verification_file=os.path.join(dataset_dir, "assignment_4_3.pt"),
+        ),
+        Assignment(
+            name="assignment_4_4",
+            test_func=test_assignment_4_4,
+            verification_file=os.path.join(dataset_dir, "assignment_4_4.pt"),
+        ),
+    ]
 
 
 def test():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--seed", type=int, default=69)
+    parser.add_argument("--generate", action="store_true")
+    parser.add_argument("--brute", action="store_true")
+
+    args = parser.parse_args()
+
+    if args.brute:
+        dataset_dir = os.path.join(PROJECT_DIR, "data", "brute_tests")
+    else:
+        dataset_dir = os.path.join(PROJECT_DIR, "data", "tests")
+
     print("\n================= TESTING ASSIGNMENTS =================\n")
-    for assignment in ASSIGNMENTS:
-        results = assignment.test_assignment(generate=GENERATE, seed=SEED)
+    for assignment in create_assignments(dataset_dir):
+        results = assignment.test_assignment(generate=args.generate, seed=args.seed)
         print(f"{assignment.name}: \n\t{results['message']}\n")
 
 
